5c1c5cfc4577636e3e98eba132cf77d8fd05d940
Bernardo González Araya <bgonzaleza003@ulacit.ed.cr>
2018-04-25 14:02:36 -0600

    Configuraciones de la base de datos agregadas

--- Controladores/db_config.php
+++ Controladores/db_config.php
@@ -1,6 +1,5 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'test');
-define('DB_DATABASE', 'test');
+define('DB_USERNAME', 'refugio_animal');
+define('DB_PASSWORD', 'oracle123');
+define('DB_CONN_STRING', 'fedora-bd2');
 ?>
